30d896a9f4d0b159f9dbca1b2fd06776d4655279
Dima Ovcharenko <d.ovcharenko90@gmail.com>
2025-05-29 18:26:31 +0300

    add params

--- src/index.js
+++ src/index.js
@@ -106,7 +106,7 @@ function installLinux() {
     value.push('apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF');
     value.push('echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | tee /etc/apt/sources.list.d/mono-official.list');
     value.push('apt-get update');
-    value.push('apt-get install mono-complete mono-devel');
+    value.push('apt-get install -y --no-install-recommends mono-complete mono-devel');
     value.push('mv ovm.exe /usr/local/bin/');
 
     let cmd = 'mono /usr/local/bin/ovm.exe "$@"';
